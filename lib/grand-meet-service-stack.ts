3354447c31e033429051b37d2f4be0da8128eac6
Jennifer Phan <j.phan@newmotion.com>
2021-03-21 09:10:11 +0100

    initial setup for aws cognito user pool

--- lib/grand-meet-service-stack.ts
+++ lib/grand-meet-service-stack.ts
@@ -1,9 +1,34 @@
 import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
 
 export class GrandMeetServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pool = new cognito.UserPool(this, 'grandmeet', {
+      userPoolName: 'grandmeet-userpool',
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: 'Verify your email for GrandMeet!',
+        emailBody: 'Hello {username}, Thanks for signing up to GrandMeet! Your verification code is {####}',
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      userInvitation: {
+        emailSubject: 'Invite to join GrandMeet!',
+        emailBody: 'Hello {username}, you have been invited to join GrandMeet! Your temporary password is {####}'
+      },
+      signInAliases: {
+        email: true
+      },
+        customAttributes: {
+          'joinedOn': new cognito.DateTimeAttribute(),
+    },
+      passwordPolicy: {
+        minLength: 12
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY
+  });
+
+    pool.addClient('grandmeet-app-client');
   }
 }
